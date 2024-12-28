27dfc438b62c375aeee50f58aad4251065d7a280
Sul Kamara <sulaimankamara110@gmail.com>
2024-12-28 20:06:06 +0000

    First commit.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Oxanium } from "next/font/google";
+import { Outfit } from "next/font/google";
+
+export const oxanium = Oxanium({ subsets: ["latin"] });
+export const outfit = Outfit({ subsets: ["latin"] });
