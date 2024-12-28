211150a5b2cf82e8571901608ddfe600588b6c3c
Sul Kamara <sulaimankamara110@gmail.com>
2024-12-28 20:27:29 +0000

    Add Lusitana font to fonts file.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,7 @@
 import { Oxanium } from "next/font/google";
 import { Outfit } from "next/font/google";
+import { Lusitana } from "next/font/google";
 
 export const oxanium = Oxanium({ subsets: ["latin"] });
 export const outfit = Outfit({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
