4a2be2d28570690d23cf9d4394ecbf5d36e4bea5
Sul Kamara <sulaimankamara110@gmail.com>
2024-12-29 20:29:06 +0000

    Revert "Add Lusitana font to fonts file."
    This reverts commit 211150a5b2cf82e8571901608ddfe600588b6c3c.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,5 @@
 import { Oxanium } from "next/font/google";
 import { Outfit } from "next/font/google";
-import { Lusitana } from "next/font/google";
 
 export const oxanium = Oxanium({ subsets: ["latin"] });
 export const outfit = Outfit({ subsets: ["latin"] });
-export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
